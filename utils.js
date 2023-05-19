cf4a5067e27c2a9e1322763f5dcecda8954314a7
mgut8184 <mgut8184@sydney.edu.au>
2023-05-19 17:47:27 +1000

    Added a title and other code in the head

--- utils.js
+++ utils.js
@@ -10,7 +10,7 @@ function yellow() {
   return `rgb(${r3}, ${g3}, ${b3})`;
 }
 
-function gray(r, g, b) {
+function gray(r, g, b) { // maybe this can be makeRGB() but only with 2 colors
   // Process
   let r4 = "222";
   let g4 = "214";
@@ -22,7 +22,7 @@ function gray(r, g, b) {
 
 function randomIntFromInterval(min, max) {
   // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function initializeSvg() {
@@ -42,27 +42,32 @@ function createPacman(svg) {
   return pacman;
 }
 
-// this can be improved:
-// - check if x and y are already overlapping an existing obstacle.
-// - random shapes / sizes
+// This generates different sized squares
 function generateRandomObstacle(svg, obstacleCoords) {
-  const obstacleWidth = 50;
-  const obstacleHeight = 50;
+  let obstacleSize = randomIntFromInterval(10,20);
+
   let obstacle = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-  obstacle.setAttribute("width", obstacleWidth);
-  obstacle.setAttribute("height", obstacleHeight);
-  const x = randomIntFromInterval(40, width);
-  const y = randomIntFromInterval(40, height);
+  obstacle.setAttribute("width", obstacleSize);
+  obstacle.setAttribute("height", obstacleSize);
+
+  // Positions
+  const x = randomIntFromInterval(100, width);
+  const y = randomIntFromInterval(100, height);
   obstacle.setAttribute("x", x);
   obstacle.setAttribute("y", y);
-  obstacle.setAttribute("fill", gray());
-  const widthPadding = obstacleWidth * 0.7;
-  const heightPadding = obstacleHeight * 0.7;
+
+  // Color of the people
+  obstacle.setAttribute("fill", gray()); // I think this can be makeRGB() but limit the RGB code to just 2 colors
+
+  // Is this padding that makes the character hit the squares?
+  const widthPadding = obstacleSize * 0.7;
+  const heightPadding = obstacleSize * 0.7;
+  
   obstacleCoords.push({
     xMin: x - widthPadding,
-    xMax: x + obstacleWidth + widthPadding,
+    xMax: x + obstacleSize + widthPadding,
     yMin: y - heightPadding,
-    yMax: y + obstacleHeight + heightPadding,
+    yMax: y + obstacleSize + heightPadding,
   });
   svg.appendChild(obstacle);
   return obstacleCoords;
