7c8537cabd9ef4ae30afed93063a748671a1b68f
Emilia <ecol4042@uni.sydney.edu.au>
2023-05-31 10:27:46 +1000

    change the square to a class

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ let obstacleCoords = [];
 //I created two forloops each 15 of different colors. 
 let svg = initializeSvg();
 let pacman = createPacman(svg);
-for (let i = 0; i < 15; i++)  {
-  obstacleCoords = generateRandomObstacle(svg, obstacleCoords);
-  obstacleCoords.setAttribute("fill", yellow());
+
+// Generate yellow obstacles
+for (let i = 0; i < 15; i++) {
+  let obstacle = generateRandomObstacle(svg, width, height);
+  obstacle.setColor("yellow");
+  obstacleCoords.push(obstacle);
 }
-for (let i = 0; i < 15; i++)  {
-  obstacleCoords = generateRandomObstacle(svg, obstacleCoords);
-  obstacleCoords.setAttribute("fill", blue());
+
+// Generate blue obstacles
+for (let i = 0; i < 15; i++) {
+  let obstacle = generateRandomObstacle(svg, width, height);
+  obstacle.setColor("blue");
+  obstacleCoords.push(obstacle);
 }
 
 let x = 50;
@@ -26,6 +32,88 @@ let dx = 4;
 let dy = 4;
 let direction = "horiz";
 
+function animate() {
+  if (direction == "horiz") {
+    x += dx;
+  } else {
+    y += dy;
+  }
+  pacman.setAttribute("cx", x);
+  pacman.setAttribute("cy", y);
+
+  if (x < 20 || x > width - 20) {
+    dx = -dx;
+  }
+
+  if (y < 20 || y > height - 20) {
+    dy = -dy;
+  }
+
+  let insideYellowObstacle = false;
+  let insideBlueObstacle = false;
+
+  for (let i = 0; i < obstacleCoords.length; i++) {
+    let obstacle = obstacleCoords[i];
+
+    if (
+      obstacle.xMin < x &&
+      obstacle.xMax > x &&
+      obstacle.yMin < y &&
+      obstacle.yMax > y
+    ) {
+      if (direction == "horiz") {
+        dx = -dx;
+      } else {
+        dy = -dy;
+      }
+
+      if (obstacle.color === "yellow") {
+        insideYellowObstacle = true;
+      } else if (obstacle.color === "blue") {
+        insideBlueObstacle = true;
+      }
+    }
+  }
+
+  // Change color based on obstacle color
+  if (insideYellowObstacle) {
+    pacman.setAttribute("fill", "yellow");
+  } else if (insideBlueObstacle) {
+    pacman.setAttribute("fill", "blue");
+  }
+
+  // change direction randomly:
+  const dir = randomIntFromInterval(1, 1000);
+  if (dir < 10) {
+    if (direction == "horiz") {
+      direction = "vert";
+    } else {
+      direction = "horiz";
+    }
+  }
+
+  requestAnimationFrame(animate);
+}
+
+animate();
+
+
+
+
+
+
+/*
+for (let i = 0; i < 15; i++)  {
+  obstacleCoords = generateRandomObstacle(svg, obstacleCoords);
+  obstacleCoords.setAttribute("fill", yellow());
+}
+for (let i = 0; i < 15; i++)  {
+  obstacleCoords = generateRandomObstacle(svg, obstacleCoords);
+  obstacleCoords.setAttribute("fill", blue());
+}
+*/
+
+/*
 function animate() {
   if (direction == "horiz") {
     x += dx;
@@ -73,3 +161,4 @@ function animate() {
 }
 
 animate();
+*/
