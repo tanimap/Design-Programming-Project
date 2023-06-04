022cb3e05efe285488771f2f566b52616c7171ea
tanimap <133322583+tanimap@users.noreply.github.com>
2023-06-04 07:56:24 +0530

    Fixed color glitch, added tittle, responsive

--- script.js
+++ script.js
@@ -1,76 +1,94 @@
 "use strict";
 
-let gameContainer = document.getElementById("grid-item");
+let animationId;
 
-let width = gameContainer.clientWidth;
-let height = gameContainer.clientHeight;
+function createGame() {
+  let gameContainer = document.getElementById("grid-item");
 
-let svg = initializeSvg();
-let pacman = createPacman(svg);
-let obstacles = [];
+  let width = gameContainer.clientWidth;
+  let height = gameContainer.clientHeight;
 
-for (let i = 0; i < 50; i++) {
-  obstacles.push(generateRandomObstacle(svg, width, height));
-}
+  let svg = initializeSvg(width, height);
+  let pacman = createPacman(svg);
+  let obstacles = [];
 
-let direction = Math.random() < 0.5 ? "horiz" : "vert";
-let dx = randomIntFromInterval(3, 7); // controls speed
-let dy = randomIntFromInterval(3, 7); // controls speed
-let x = parseInt(pacman.getAttribute("cx"));
-let y = parseInt(pacman.getAttribute("cy"));
-
-function animate() {
-  if (direction === "horiz") {
-    x += dx;
-  } else {
-    y += dy;
-  }
-  pacman.setAttribute("cx", x);
-  pacman.setAttribute("cy", y);
-
-  if (x < 20 || x > width - 20) {
-    dx = -dx;
+  for (let i = 0; i < 50; i++) {
+    obstacles.push(generateRandomObstacle(svg, width, height));
   }
 
-  if (y < 20 || y > height - 20) {
-    dy = -dy;
-  }
+  let direction = Math.random() < 0.5 ? "horiz" : "vert";
+  let dx = randomIntFromInterval(3, 7); // controls speed
+  let dy = randomIntFromInterval(3, 7); // controls speed
+  let x = parseInt(pacman.getAttribute("cx"));
+  let y = parseInt(pacman.getAttribute("cy"));
 
-  // This detects collisions and will allow circle to change colors whenever it collides
-  let collisionDetected = false;
-
-  for (let i = 0; i < obstacles.length; i++) {
-    let obstacle = obstacles[i];
-
-    if (obstacle.isPointInside(x, y)) {
-      collisionDetected = true;
-
-      if (obstacle.obstacle.getAttribute("fill") === blue()) {
-        pacman.setAttribute("fill", blue());
-      } else if (obstacle.obstacle.getAttribute("fill") === yellow()) {
-        pacman.setAttribute("fill", yellow());
-      }
-
-      obstacle.rearrangeObstacles();
-
-      if (direction === "horiz") {
-        dx = -dx;
-      } else {
-        dy = -dy;
-      }
-    }
-  }
-
-  const dir = randomIntFromInterval(1, 1000);
-  if (dir < 10) {
+  function animate() {
     if (direction === "horiz") {
-      direction = "vert";
+      x += dx;
     } else {
-      direction = "horiz";
+      y += dy;
     }
-  }
+    pacman.setAttribute("cx", x);
+    pacman.setAttribute("cy", y);
 
-  requestAnimationFrame(animate);
+    if (x < 20 || x > width - 20) {
+      dx = -dx;
+    }
+
+    if (y < 20 || y > height - 20) {
+      dy = -dy;
+    }
+
+    // This detects collisions and will allow circle to change colors whenever it collides
+    let collisionDetected = false;
+
+    for (let i = 0; i < obstacles.length; i++) {
+      let obstacle = obstacles[i];
+
+      if (obstacle.isPointInside(x, y)) {
+        collisionDetected = true;
+
+        if (obstacle.obstacle.getAttribute("fill") === blue()) {
+          pacman.setAttribute("fill", blue());
+        } else if (obstacle.obstacle.getAttribute("fill") === yellow()) {
+          pacman.setAttribute("fill", yellow());
+        }
+
+        obstacle.rearrangeObstacles();
+
+        if (direction === "horiz") {
+          dx = -dx;
+        } else {
+          dy = -dy;
+        }
+      }
+    }
+
+    const dir = randomIntFromInterval(1, 1000);
+    if (dir < 10) {
+      if (direction === "horiz") {
+        direction = "vert";
+      } else {
+        direction = "horiz";
+      }
+    }
+
+    animationId = requestAnimationFrame(animate);
+  }
+  animate();
+  return svg;
 }
 
-animate();
+let svg = createGame();
+
+function reanimate() {
+  // clear the animation of our old game:
+  cancelAnimationFrame(animationId);
+
+  // wipe out our old game contents (rects, pacman, etc):
+  while (svg.firstChild) {
+    svg.removeChild(svg.firstChild);
+  }
+  svg = createGame();
+}
+window.addEventListener("resize", reanimate);
